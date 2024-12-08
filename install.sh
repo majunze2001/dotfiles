49e1839ebe60d46380b059c50acad6147d1bd8ae
Jeff Ma <jeffjma@umich.edu>
2024-12-08 17:09:40 -0500

    fix install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ if [ -n "$SSH" ]; then
 else
   git clone --bare https://github.com/majunze2001/dotfiles.git $HOME/.dotfiles
 fi
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout main
 
-dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' checkout unified
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout unified
 bash $HOME/.dotmodules/install/all.sh
