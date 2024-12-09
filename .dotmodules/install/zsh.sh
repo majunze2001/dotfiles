d2ef933acc3207f611341d6d8455f5fb6f8f3e1d
Jeff Ma <jeffjma@umich.edu>
2024-12-09 11:10:34 -0500

    avoid duplicate isntallation of zsh-related tools

--- .dotmodules/install/zsh.sh
+++ .dotmodules/install/zsh.sh
@@ -10,25 +10,50 @@ installing() {
   pprint "#################################################"
 }
 
-installing "Oh-my-zsh"
-export RUNZSH=no
-export KEEP_ZSHRC=yes
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-source ~/.zshrc
-
-installing "Powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
-
-installing "fast-syntax-highlighting"
-git clone --depth=1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
-
-installing "zsh-autosuggestions"
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-
-installing "direnv"
-mkdir -p ~/.local/bin
-if [[ $(uname -s) = Darwin ]]; then
-  brew install direnv
+# Install Oh-my-zsh
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  installing "Oh-my-zsh"
+  export RUNZSH=no
+  export KEEP_ZSHRC=yes
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  source ~/.zshrc
 else
-  curl -sfL https://direnv.net/install.sh | bin_path=~/.local/bin bash
+  pprint "Oh-my-zsh is already installed. Skipping."
+fi
+
+# Install Powerlevel10k
+if [[ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]]; then
+  installing "Powerlevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+else
+  pprint "Powerlevel10k is already installed. Skipping."
+fi
+
+# Install fast-syntax-highlighting
+if [[ ! -d "$ZSH_CUSTOM/plugins/fast-syntax-highlighting" ]]; then
+  installing "fast-syntax-highlighting"
+  git clone --depth=1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
+else
+  pprint "fast-syntax-highlighting is already installed. Skipping."
+fi
+
+# Install zsh-autosuggestions
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]]; then
+  installing "zsh-autosuggestions"
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+else
+  pprint "zsh-autosuggestions is already installed. Skipping."
+fi
+
+# Install direnv
+if command -v direnv >/dev/null 2>&1; then
+  pprint "direnv is already installed. Skipping."
+else
+  installing "direnv"
+  mkdir -p ~/.local/bin
+  if [[ $(uname -s) = Darwin ]]; then
+    brew install direnv
+  else
+    curl -sfL https://direnv.net/install.sh | bin_path=~/.local/bin bash
+  fi
 fi
