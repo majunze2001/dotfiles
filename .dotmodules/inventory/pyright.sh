2ce1514b6d95101a6d8e7938149be1669ebc48df
Jeff Ma <jeffjma@umich.edu>
2025-04-02 20:02:04 -0400

    use nvm

--- .dotmodules/inventory/pyright.sh
+++ .dotmodules/inventory/pyright.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-VERSION=${VERSION:-"22.9.0"}
+set -euo pipefail
+
+VERSION=${VERSION:-"23"}
 
 if [[ $(uname -m) = arm64 ]]; then
   ARCH=arm64
@@ -12,10 +14,15 @@ KERNEL="$(uname | awk '{print tolower($0)}')"
 
 if ! command -v node 2>&1 > /dev/null; then
   cd /tmp
-  curl -LO "https://nodejs.org/dist/v$VERSION/node-v$VERSION-$KERNEL-$ARCH.tar.xz"
-  mkdir -p ~/.local/src
-  tar xJf "node-v$VERSION-$KERNEL-$ARCH.tar.xz" -C ~/.local/src
-  mv ~/.local/src/node-* ~/.local/src/node
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  nvm install $VERSION
+  nvm use $VERSION
+  if ! command -v node 2>&1 > /dev/null; then
+    echo "NVM install failed"
+    exit 1
+  fi
 fi
 
 npm install -g pyright
