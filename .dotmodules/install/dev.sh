552bf7eebf69015c9e223af6183d31edba1068f5
Jeff Ma <jeffjma@umich.edu>
2026-07-07 18:57:25 +0000

    dev tools

---
+++ .dotmodules/install/dev.sh
@@ -0,0 +1,11 @@
+# install claude code
+curl -fsSL https://claude.ai/install.sh | bash
+
+# install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# install gh cli
+curl -sS https://webi.sh/gh | sh
+
+# install codex
+curl -fsSL https://chatgpt.com/codex/install.sh | CODEX_NON_INTERACTIVE=1 sh
